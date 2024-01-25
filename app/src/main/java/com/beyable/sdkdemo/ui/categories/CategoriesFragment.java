34976ed499eed5ab76bc1e8092159d4ca5f30d9f
Marco Pereira <marco.pereira@tivine.com>
2024-01-25 10:16:03 +0000

    Initial commit

---
+++ app/src/main/java/com/beyable/sdkdemo/ui/categories/CategoriesFragment.java
@@ -0,0 +1,37 @@
+package com.beyable.sdkdemo.ui.categories;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.beyable.sdkdemo.databinding.FragmentCategoriesBinding;
+
+public class CategoriesFragment extends Fragment {
+
+    private FragmentCategoriesBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        CategoriesViewModel dashboardViewModel =
+                new ViewModelProvider(this).get(CategoriesViewModel.class);
+
+        binding = FragmentCategoriesBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textDashboard;
+        dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
