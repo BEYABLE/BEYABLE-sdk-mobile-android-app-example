f6864aed66504aaec667c2f95e57a2e8bcb1424a
Marco Pereira <marco.pereira@tivine.com>
2024-02-09 09:19:46 +0000

    Added login button on home. Send login informations to Beyable SDK

--- app/src/main/java/com/beyable/sdkdemo/ui/home/HomeFragment.java
+++ app/src/main/java/com/beyable/sdkdemo/ui/home/HomeFragment.java
@@ -1,9 +1,11 @@
 package com.beyable.sdkdemo.ui.home;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -11,6 +13,8 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.beyable.beyable_sdk.Beyable;
 import com.beyable.beyable_sdk.models.BYHomeAttributes;
+import com.beyable.beyable_sdk.models.BYVisitorInfos;
+import com.beyable.sdkdemo.R;
 import com.beyable.sdkdemo.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
@@ -24,6 +28,25 @@ public class HomeFragment extends Fragment {
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        Button logButton = binding.logButton;
+        // Check if user is connected (we keep a flag on SharePreferences)
+        SharedPreferences pref = getContext().getSharedPreferences("beyable_demo_preferences", 0);
+        boolean isLogged = pref.getBoolean("is_logged", false);
+        handleButtonText(logButton, isLogged);
+        handleLoginStatus(isLogged);
+
+        logButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                boolean newLogStatus = !isLogged;
+                SharedPreferences pref = getContext().getSharedPreferences("beyable_demo_preferences", 0);
+                SharedPreferences.Editor editor = pref.edit();
+                editor.putBoolean("is_logged", newLogStatus);
+                editor.apply();
+                handleButtonText(logButton, newLogStatus);
+                handleLoginStatus(newLogStatus);
+            }
+        });
 
         sendPageViewToBeyable(root);
         return root;
@@ -39,6 +62,24 @@ public class HomeFragment extends Fragment {
         // CALL Beyable SDK to inform that we are viewing the home page
         BYHomeAttributes attributes = new BYHomeAttributes();
         Beyable.getSharedInstance().sendPageView(getActivity(), "/", attributes);
+    }
+
+    private void handleButtonText(Button button, boolean isLogged) {
+        if (isLogged) {
+            button.setText(getResources().getText(R.string.home_logout_button));
+        } else {
+            button.setText(getResources().getText(R.string.home_login_button));
+        }
+    }
+
+    private void handleLoginStatus(boolean isLogged) {
+        if (isLogged) {
+            Beyable.getSharedInstance().setVisitorInfos(
+                    new BYVisitorInfos(true, true, "Gol D. Marko", "smartphones"));
+        } else {
+            // The user is now logged out. Prevent Beyable
+            Beyable.getSharedInstance().setVisitorInfos(null);
+        }
     }
 
 }
